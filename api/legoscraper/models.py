6bbca593fe2a3e06abe204e99f2c2ad3c44c5884
pietrykovsky <m.pietrykowski2001@gmail.com>
2022-08-11 21:44:13 +0200

    added LegoSet model and property test

--- api/legoscraper/models.py
+++ api/legoscraper/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+from decimal import Decimal, getcontext
+
+class LegoSet(models.Model):
+    """Model for lego set objects."""
+
+    title = models.CharField(max_length=255)
+    product_id = models.CharField(max_length=50, unique=True)
+    theme = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    available = models.BooleanField(default=False)
+    age = models.CharField(max_length=3)
+    elements = models.IntegerField()
+    link = models.TextField()
+    minifigures = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def price_per_element(self):
+        """Return price per element ratio."""
+        elements = Decimal(self.elements)
+        price = Decimal(self.price)
+        return Decimal(price/elements).quantize(Decimal('.01'))
--- api/legoscraper/tests.py
+++ api/legoscraper/tests.py
@@ -6,7 +6,7 @@ from .models import LegoSet
 
 class ModelTests(TestCase):
     """Tests for legoscraper models."""
-# name of set, price, theme, elements, product id, photo, available, age, minifigures, link
+
     def test_create_legoset_success(self):
         """Test create legoset object."""
         fields = {
@@ -18,11 +18,25 @@ class ModelTests(TestCase):
             'age': '18+',
             'elements': 2354,
             'link': 'https://www.lego.com/pl-pl/product/nasa-space-shuttle-discovery-10283',
-            'minifigures': None,
         }
-        legoset = LegoSet.objects.create(fields['title'], fields['product_id'], fields['theme'], fields['price'], fields['available'], fields['age'], fields['elements'], fields['link'], fields['minifigures'])
+        legoset = LegoSet.objects.create(title=fields['title'], product_id=fields['product_id'], theme=fields['theme'], price=fields['price'], available=fields['available'], age=fields['age'], elements=fields['elements'], link=fields['link'])
 
         for k, v in fields.items():
             self.assertEqual(getattr(legoset, k), v)
+        self.assertEqual(legoset.minifigures, None)
 
+    def test_legoset_price_per_element_correct(self):
+        """Test price per element property returns a correct value."""
+        fields = {
+            'title': 'test title',
+            'product_id': '10283',
+            'theme': 'lego-icons',
+            'price': Decimal('333.33'),
+            'available': True,
+            'age': '18+',
+            'elements': 2,
+            'link': 'https://www.lego.com/pl-pl/product/nasa-space-shuttle-discovery-10283',
+        }
+        legoset = LegoSet.objects.create(title=fields['title'], product_id=fields['product_id'], theme=fields['theme'], price=fields['price'], available=fields['available'], age=fields['age'], elements=fields['elements'], link=fields['link'])
 
+        self.assertEqual(legoset.price_per_element, Decimal('166.66'))
