a8d2e3b0aa0ece0e62eb918d365f69d6bc5091f5
pietrykovsky <m.pietrykowski2001@gmail.com>
2022-08-16 23:28:24 +0200

    updated price field

--- api/legoscraper/models.py
+++ api/legoscraper/models.py
@@ -8,7 +8,7 @@ class LegoSet(models.Model):
     title = models.CharField(max_length=255)
     product_id = models.CharField(max_length=50, unique=True)
     theme = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.CharField(max_length=100)
     age = models.CharField(max_length=5)
     elements = models.IntegerField()
