6a50dc447ae11cc982b353137001b18a630101cd
pietrykovsky <m.pietrykowski2001@gmail.com>
2022-08-28 14:17:46 +0200

    updated filterset

--- api/legoscraper/filters.py
+++ api/legoscraper/filters.py
@@ -6,10 +6,10 @@ class LegoSetFilter(filters.FilterSet):
     """Basic filter set for lego set api."""
     price = filters.RangeFilter(field_name='price')
     elements = filters.RangeFilter(field_name='elements')
-    theme = filters.CharFilter(field_name='theme__name')
-    age = filters.CharFilter(field_name='age__name')
+    theme_name = filters.CharFilter(field_name='theme__name', lookup_expr='icontains')
+    age_name = filters.CharFilter(field_name='age__name')
     minifigures = filters.RangeFilter(field_name='minifigures')
 
     class Meta:
         model = LegoSet
-        fields = ['available']
+        fields = ['available', 'theme', 'age']
